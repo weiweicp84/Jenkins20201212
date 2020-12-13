08edd97bd46c6cd13e2fd27069154bd2afe4bad5
weiweicp84 <weiweicp@qq.com>
2020-12-13 22:42:38 +0800

    first

---
+++ restapi-teach/install.sh
@@ -0,0 +1,5 @@
+
+
+pip3 install django==2.0.4 -i http://pypi.douban.com/simple/  --trusted-host pypi.douban.com
+pip3 install cherrypy -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com
+pause 
---
+++ restapi-teach/run.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+cd bankend  && nohup  &&  nohup python3 project/cherrypy_startup.py >& run.log &
+
+exit 0
+
+ 
+ 
+
