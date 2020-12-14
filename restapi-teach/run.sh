150bd26d3c9ca26ac5909ec6cfcdb06cb5e56780
weiweicp84 <weiweicp@qq.com>
2020-12-14 21:57:46 +0800

    update

--- restapi-teach/run.sh
+++ restapi-teach/run.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 pwd
 
-cd autodeploy/restapi-teach  &&  nohup python3 project/cherrypy_startup.py >& run.log &
+cd bankend  &&  nohup python3 project/cherrypy_startup.py >& run.log &
 
 exit 0
 
