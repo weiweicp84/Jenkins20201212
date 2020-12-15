ff22d0246162bf5b5b8a24de1c65149ef25153d8
weiweicp84 <weiweicp@qq.com>
2020-12-15 22:40:15 +0800

    update

--- restapi-teach/run.sh
+++ restapi-teach/run.sh
@@ -2,9 +2,9 @@
 pwd
 
 
-//如果分开写命令，如下则会在执行cherrypy_startup.py后，jenkins一直阻塞在这
-//cd backend
-//python3 project/cherrypy_startup.py
+#如果分开写命令，如下则会在执行cherrypy_startup.py后，jenkins一直阻塞在这
+#cd backend
+#python3 project/cherrypy_startup.py
 source /etc/profile
 cd backend  && nohup python3 project/cherrypy_startup.py & > run.log &
 
