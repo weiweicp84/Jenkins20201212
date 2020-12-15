c062ba713b0c65b78f289113f1e1c48c73df0757
weiweicp84 <weiweicp@qq.com>
2020-12-15 23:12:36 +0800

    update

--- restapi-teach/run.sh
+++ restapi-teach/run.sh
@@ -7,8 +7,14 @@ pwd
 #python3 project/cherrypy_startup.py
 source /etc/profile
 
+export BUILD_ID=anyWordIsOk
+
 echo JAVA_HOME is $JAVA_HOME
 echo PATH is $PATH
-cd backend  && nohup python3 project/cherrypy_startup.py & > run.log &
+cd backend  && nohup python3 project/cherrypy_startup.py && sleep & > run.log &
+
+echo 'start sleep...'
+sleep 10s
+echo 'end sleep...'
 
 exit 0
